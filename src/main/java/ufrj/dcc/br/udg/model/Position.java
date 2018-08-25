e88773359f1334f3640a396da328117dc2e641ee
Rodrigo Ney <rodrigo.ney@nubank.com.br>
2018-08-25 00:02:03 -0300

    Adding equals and hashCode to position

--- src/main/java/ufrj/dcc/br/udg/model/Position.java
+++ src/main/java/ufrj/dcc/br/udg/model/Position.java
@@ -21,7 +21,26 @@ public class Position {
 	public void setY(double y) {
 		this.y = y;
 	}
-	
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == this) return true;
+        if (!(o instanceof Position)) {
+            return false;
+        }
+
+        Position pos = (Position) o;
+        return this.x == pos.x && this.y == pos.y;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = 17;
+        result = 31 * result + (int) (x * 100000);
+        result = 27 * result + (int) (y * 100000000);
+        return result;
+    }
+
 	@Override
 	public String toString() {
 		return "(x,y) = (" + x + ", " + y + ")";
